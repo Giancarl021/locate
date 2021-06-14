4c7954b73a0ddfd71f22d02605f39839a4942b1c
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-06-14 15:23:04 -0300

    Started project

---
+++ index.js
@@ -0,0 +1,14 @@
+const { homedir } = require('os');
+const path = require('path');
+
+const root = path.dirname(require.main.filename);
+
+module.exports = function(pathToFile, useCWD = false) {
+    if (!pathToFile) throw new Error('Invalid path');
+
+    const _path = pathToFile.replace(/(~|%userprofile%|%home%)/gi, homedir()).replace(/\\/g, '/');
+
+    if(path.isAbsolute(_path)) return _path;
+    
+    return (useCWD ? path.resolve(process.cwd(), _path) : path.resolve(root, _path)).replace(/\\/g, '/');
+};
