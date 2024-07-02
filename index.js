3698c58192aa6437f63a5513a1f46f6e8d50f3f3
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2024-07-02 10:16:27 -0300

    fix: path.resolve on absolute paths too

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const root = path.dirname(require.main.filename);
 
-module.exports = function(pathToFile, useCWD = false) {
+module.exports = function (pathToFile, useCWD = false) {
     if (!pathToFile) throw new Error('Invalid path');
 
     const _path = pathToFile.replace(/(~|%userprofile%|%home%)/gi, homedir());
 
-    if(path.isAbsolute(_path)) return _path;
-    
-    return (useCWD ? path.resolve(process.cwd(), _path) : path.resolve(root, _path));
+    if (path.isAbsolute(_path)) return path.resolve(_path);
+
+    return useCWD
+        ? path.resolve(process.cwd(), _path)
+        : path.resolve(root, _path);
 };
